1add3941bd968cec2f7c6061968c522b5ec65916
Jason A. Martin <jason.martin@jasonamartin.com>
2018-03-12 07:40:17 -0700

    INIT: initial commit.

---
+++ Assets/Scripts/CEO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CEO", menuName = "AdventuresOnWallStreet/CEO", order = 1)]
+public class CEO : ScriptableObject
+{
+    public float compentcyScore;
+    public float employmentTurns;
+}
---
+++ Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExampleScript : MonoBehaviour {
+    public string PlayerName = "Jason";
+    public Button myButton;
+	// Use this for initialization
+	void Start () {
+        Button btn = myButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TaskOnClick()
+    {
+        print("The player's name is: " + PlayerName);
+    }
+}
