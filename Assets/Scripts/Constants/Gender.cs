3a4b3d7c6002f8604ac10ff669934f2028d759ca
Jason A. Martin <jason.martin@jasonamartin.com>
2018-03-13 18:52:55 -0700

    refactor: updating project file structure and adding Unity asset bundler add-in.

--- Assets/Scripts/Constants/Gender.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Gender", menuName = "AdventuresOnWallStreet/Gender", order = 5)]
-public class Gender : ScriptableObject {
-    public string gender;
-}
