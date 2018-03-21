ae7de96f6785e28126d158d2c9b5e22e73d935ff
Jason A. Martin <jason.martin@jasonamartin.com>
2018-03-20 21:34:10 -0700

    feature: Adding event types, events and other SOs.

---
+++ Assets/_Project/Scripts/Constants/EventType.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EventType", menuName = "AdventuresOnWallStreet/EventType", order = 30)]
+public class EventType : ScriptableObject {
+   // USE AS ENUM
+}
---
+++ Assets/_Project/Scripts/Constants/GoodsType.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GoodsType", menuName = "AdventuresOnWallStreet/GoodsType", order = 30)]
+public class GoodsType : ScriptableObject {
+    // use as ENUM for now.
+}
