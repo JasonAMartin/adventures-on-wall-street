636298761a73fd553beda05822ef07a527c79da4
Jason A. Martin <jason.martin@jasonamartin.com>
2018-04-01 11:21:01 -0700

    feature: Added CompanyList SO and build tool to create it.

---
+++ Assets/Editor/BuildTools/BuildCompanyList.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class BuildCompanyList
+{
+    [MenuItem("Assets/Create/Build Tools/BuildCompanyList")]
+    public static void Create()
+    {
+        string buildDirectory = "Assets/_Project/Scripts/ScriptableObjects/Company/List";
+
+        List<Company> companies = new List<Company>();
+
+        string[] allCompanies = AssetDatabase.FindAssets("t:company", null);
+ 
+        
+        // populate lists
+        foreach (string guid in allCompanies)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            companies.Add(AssetDatabase.LoadAssetAtPath<Company>(path));
+        }
+
+        CompanyList asset = ScriptableObject.CreateInstance<CompanyList>();
+        asset.companyList = companies;
+        AssetDatabase.CreateAsset(asset, buildDirectory + "/companyList.asset");
+        AssetDatabase.SaveAssets();
+        Debug.Log("Company List Build Completed.");
+    }
+
+}
---
+++ Assets/_Project/Scripts/Constants/Lists/CompanyList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CompanyList", menuName = "AdventuresOnWallStreet/CompanyList", order = 19)]
+public class CompanyList : ScriptableObject
+{
+    public List<Company> companyList = new List<Company>();
+}
