fd29d611a2266b1d0096f8ea6d8598101563e1a3
Jason A. Martin <jason.martin@jasonamartin.com>
2018-04-03 07:30:28 -0700

    feature: Added stock symbol checker in BuildCompanies to ensure there's no conflic. Also updated companies JSON with more seed data.

--- Assets/Editor/BuildTools/BuildCompanies.cs
+++ Assets/Editor/BuildTools/BuildCompanies.cs
@@ -21,7 +21,7 @@ public class BuildCompanies
     public static void Create()
     {
 
-        // TODO add company strength as well. If it's NULL, leave alone, otherwise add the SO
+        // TODO there's a stock symbol check in here. Make a reporter script that looks into created SOs to make sure no conflict exists past this point. Do other reports/tests as well.
 
         string buildDirectory = "Assets/_Project/Scripts/ScriptableObjects/Company";
 
@@ -29,6 +29,7 @@ public class BuildCompanies
         CompanyJSON[] companies = JSONHelper.getJsonArray<CompanyJSON>(dataAsJson);
         string[] companyTypesGUID = AssetDatabase.FindAssets("t:companytype", null);
         List<CompanyType> companyTypes = new List<CompanyType>();
+        List<Company> createdCompanies = new List<Company>();
 
         // populate company types list
         foreach (string guid in companyTypesGUID)
@@ -36,6 +37,7 @@ public class BuildCompanies
             string path = AssetDatabase.GUIDToAssetPath(guid);
             companyTypes.Add(AssetDatabase.LoadAssetAtPath<CompanyType>(path));
         }
+
 
         foreach (CompanyJSON company in companies)
         {
@@ -53,10 +55,18 @@ public class BuildCompanies
                 if (cType.companyType == company.companyType) companySO.companyType = cType;
             }
 
+            // CHECK if Stock Symbol is in createdCompanies yet. If so, report it.
+            foreach (Company c in createdCompanies)
+            {
+                if (c.stockSymbol == companySO.stockSymbol) Debug.Log("CONFLICT!!: " + c.companyName + " && " + companySO.companyName);
+            }
+
+            createdCompanies.Add(companySO);
+
             AssetDatabase.CreateAsset(companySO, buildDirectory + "/Company-" + companySO.companyName.Replace(" ","") + ".asset");
 
-            // Save Asset
-            AssetDatabase.SaveAssets();
+           // Save Asset
+           AssetDatabase.SaveAssets();
         }
 
         Debug.Log("COMPLETED! Built " + companies.Length + " Companies.");
