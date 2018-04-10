1b2bf467cd937891f4aff5193246b578ca4a9e51
Jason A. Martin <jason.martin@jasonamartin.com>
2018-04-09 17:58:13 -0700

    WIP: working on stock price levels.

---
+++ Assets/_Project/Scripts/Constants/StockPriceLevel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StockPriceLevel", menuName = "AdventuresOnWallStreet/StockPriceLevel", order = 55)]
+public class StockPriceLevel : ScriptableObject {
+    public int minimumStartingPrice;
+    public int maximimStartingPrice;
+}
